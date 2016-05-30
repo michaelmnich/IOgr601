15105f0b354ed8e00a4fe430cf87569d26bc31e5
lopl233 <lopl233@o2.pl>
2016-05-30 15:32:31 +0200

    123

---
+++ matrixlibrary/src/IMatrixTest.java
@@ -0,0 +1,22 @@
+
+import org.junit.Test;
+
+public class IMatrixTest{
+
+    @Test
+    public void testCreatIdentityMatrix(){}
+
+    @Test
+    public void testDeterminant(){}
+
+    @Test
+    public void testGetMatrixValue (){}
+
+    @Test
+    public void testsetMatrixValue (){}
+
+    @Test
+    public void testsetMatrixValues (){}
+
+
+}
