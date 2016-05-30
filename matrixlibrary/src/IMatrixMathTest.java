53095a4acf2100c44bfca30d7c061c0805e6ec1d
Ph0ndragX <ph0ndragxdev@gmail.com>
2016-05-30 15:24:09 +0200

    Tests definition for IMatrixMath

---
+++ matrixlibrary/src/IMatrixMathTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+
+/**
+ * Created by z1091308 on 2016-05-30.
+ */
+
+public class IMatrixMathTest {
+
+    @Test
+    public void testInverseMatrix() {
+
+    }
+
+    @Test
+    public void testMatrixAddition() {
+
+    }
+
+    @Test
+    public void testMatrixMultiplication() {
+
+    }
+
+    @Test
+    public void testMatrixSubtracting() {
+
+    }
+
+    @Test
+    public void testMatrixTransposition() {
+
+    }
+
+    @Test
+    public void testScalarMultiplication() {
+
+    }
+}
