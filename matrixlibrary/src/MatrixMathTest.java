840b900de03264ebb142ffa087676cc24c4c5c99
lopl233 <lopl233@o2.pl>
2016-05-30 16:52:27 +0200

    wszystkie testy

--- matrixlibrary/src/MatrixMathTest.java
+++ matrixlibrary/src/MatrixMathTest.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 
 public class MatrixMathTest {
     @Test
-    void testInverseMatrix(){}
+    public void testInverseMatrix(){}
 
     @Test
-    void testMatrixAddition(){
+    public void testMatrixAddition(){
         Matrix macierz1=new Matrix();
         Matrix macierz2=new Matrix();
         double tablica1[][]=new double[3][3];
@@ -21,22 +21,61 @@ public class MatrixMathTest {
         macierz1.setMatrixValues(tablica1);
         macierz2.setMatrixValues(tablica2);
         MatrixMath TEST=new MatrixMath();
+        IMatrix macierz3=new Matrix();
 
+        try {
+            macierz3=TEST.MatrixAddition(macierz1,macierz2);
+        } catch (Exception e) {org.junit.Assert.fail();
+        }
+
+        for(int a=0;a<3;a++)
+            for(int b=0;b<3;b++){
+                try { Assert.assertEquals(macierz3.GetMatrixValue(a,b),(double)2*a);
+                } catch (Exception e) {org.junit.Assert.fail(); }
+            }
 
 
     }
 
     @Test
-    void testMatrixMultiplication(){}
+    public void testMatrixMultiplication(){}
 
     @Test
-    void testMatrixSubtracting(){}
+    public void testMatrixSubtracting(){
+        Matrix macierz1=new Matrix();
+        Matrix macierz2=new Matrix();
+        double tablica1[][]=new double[3][3];
+        double tablica2[][]=new double[3][3];
+
+        for(int a=0;a<3;a++)
+            for(int b=0;b<3;b++){
+                tablica1[a][b]=a+b;
+                tablica2[a][b]=a-b;
+            }
+        macierz1.setMatrixValues(tablica1);
+        macierz2.setMatrixValues(tablica2);
+        MatrixMath TEST=new MatrixMath();
+        IMatrix macierz3=new Matrix();
+
+        try {
+            macierz3=TEST.MatrixSubtracting(macierz1,macierz2);
+        } catch (Exception e) {org.junit.Assert.fail();
+        }
+
+        for(int a=0;a<3;a++)
+            for(int b=0;b<3;b++){
+                try { Assert.assertEquals(macierz3.GetMatrixValue(a,b),(double)2*b);
+                } catch (Exception e) {org.junit.Assert.fail(); }
+            }
+
+
+    }
 
     @Test
-    void testMatrixTransposition(){}
+    public void testMatrixTransposition(){}
 
     @Test
-    void testScalarMultiplication(){}
+    public void testScalarMultiplication(){}
 
 }
 
